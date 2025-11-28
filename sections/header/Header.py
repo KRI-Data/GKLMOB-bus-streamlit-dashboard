cf68d2db4e649e831a86cee4dc62d9eac05bd3bb
LingShyanSeng <161465221+LingShyanSeng@users.noreply.github.com>
2025-11-28 04:41:10 +0000

    update font.

--- sections/header/Header.py
+++ sections/header/Header.py
@@ -13,7 +13,7 @@ st.markdown("""
     /* Base style (mobile-first) */
     div[data-testid="stHorizontalBlock"] .nav-link,
     div[data-testid="stHorizontalBlock"] .option-menu .nav-link {
-        font-size: 12px !important;
+        font-size: 10px !important;
     }
 
     /* Medium screens */
