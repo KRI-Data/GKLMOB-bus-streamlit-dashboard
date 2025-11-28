16b97a33d65e540f7a0687cbf58f5a0beb4e4e15
LingShyanSeng <161465221+LingShyanSeng@users.noreply.github.com>
2025-11-28 08:31:14 +0000

    test.

--- graphs/Ternary_plots.py
+++ graphs/Ternary_plots.py
@@ -7,29 +7,6 @@ from datetime import datetime
 # Add parent directory to sys.path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-st.markdown("""
-<style>
-
-/* --- Default (mobile) --- */
-.plotly .legend text {
-    font-size: 10px !important;
-}
-.plotly .hovertext text {
-    font-size: 10px !important;
-}
-
-@media (min-width: 700px) {
-    .plotly .legend text {
-        font-size: 14px !important;
-    }
-    .plotly .hovertext text {
-        font-size: 14px !important;
-    }
-}
-
-</style>
-""", unsafe_allow_html=True)
-
 # --- Reusable slider function ---
 def month_slider(unique_months_list, label="Select Month:", key=None):
     """
@@ -162,12 +139,15 @@ def get_ternary_rapid_kl(df):
             x=0,                         # position: x-axis (0=left)
             y=-0.3,                      # position: y-axis (below plot)
             xanchor='left',
-            yanchor='top'
+            yanchor='top',
+            font=dict(
+            size=12
+            )
         ),
         hoverlabel=dict(
             bgcolor='rgba(255,255,255,0.25)',  # white with 70% opacity
             bordercolor='white',               # white border (optional)
-            font=dict(color='black')           # black text
+            font=dict(size = 12, color='black')           # black text
         )      
     )
         
