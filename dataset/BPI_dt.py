9f95224a322b4a2f9589adc892548c157914722c
LingShyanSeng <161465221+LingShyanSeng@users.noreply.github.com>
2026-04-14 15:48:39 +0000

    uploaded dataset

--- dataset/BPI_dt.py
+++ dataset/BPI_dt.py
@@ -17,6 +17,27 @@ def ratio_rapid_kl():
     ratio_rapid_kl_new['ratio_early'] = ratio_rapid_kl_new['n_early'] / ratio_rapid_kl_new['n']
     ratio_rapid_kl_new['ratio_late'] = ratio_rapid_kl_new['n_late'] / ratio_rapid_kl_new['n']
 
+    df_202512 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202512.csv")
+    ratio_rapid_kl_202512 = df_202512[['route_short_name', 'route_long_name', 'month', 'n', 'n_on_time', 'n_early', 'n_late']]
+
+    ratio_rapid_kl_202512['ratio_on_time'] = ratio_rapid_kl_202512['n_on_time'] / ratio_rapid_kl_202512['n']
+    ratio_rapid_kl_202512['ratio_early'] = ratio_rapid_kl_202512['n_early'] / ratio_rapid_kl_202512['n']
+    ratio_rapid_kl_202512['ratio_late'] = ratio_rapid_kl_202512['n_late'] / ratio_rapid_kl_202512['n']
+
+    df_202601 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202601.csv")
+    ratio_rapid_kl_202601 = df_202601[['route_short_name', 'route_long_name', 'month', 'n', 'n_on_time', 'n_early', 'n_late']]
+
+    ratio_rapid_kl_202601['ratio_on_time'] = ratio_rapid_kl_202601['n_on_time'] / ratio_rapid_kl_202601['n']
+    ratio_rapid_kl_202601['ratio_early'] = ratio_rapid_kl_202601['n_early'] / ratio_rapid_kl_202601['n']
+    ratio_rapid_kl_202601['ratio_late'] = ratio_rapid_kl_202601['n_late'] / ratio_rapid_kl_202601['n']
+
+    df_202602 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202602.csv")
+    ratio_rapid_kl_202602 = df_202602[['route_short_name', 'route_long_name', 'month', 'n', 'n_on_time', 'n_early', 'n_late']]
+
+    ratio_rapid_kl_202602['ratio_on_time'] = ratio_rapid_kl_202602['n_on_time'] / ratio_rapid_kl_202602['n']
+    ratio_rapid_kl_202602['ratio_early'] = ratio_rapid_kl_202602['n_early'] / ratio_rapid_kl_202602['n']
+    ratio_rapid_kl_202602['ratio_late'] = ratio_rapid_kl_202602['n_late'] / ratio_rapid_kl_202602['n']
+
     ratio_rapid_kl = ratio_rapid_kl.round({
         'ratio_on_time': 4,
         'ratio_late': 4,
@@ -29,7 +50,25 @@ def ratio_rapid_kl():
         'ratio_early': 4
     })
 
-    combined = pd.concat([ratio_rapid_kl, ratio_rapid_kl_new], ignore_index=True)
+    ratio_rapid_kl_202512 = ratio_rapid_kl_202512.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    ratio_rapid_kl_202601 = ratio_rapid_kl_202601.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    ratio_rapid_kl_202602 = ratio_rapid_kl_202602.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    combined = pd.concat([ratio_rapid_kl, ratio_rapid_kl_new, ratio_rapid_kl_202512, ratio_rapid_kl_202601, ratio_rapid_kl_202602], ignore_index=True)
 
     return combined
 
@@ -49,6 +88,27 @@ def ratio_mrt_feeder():
     ratio_mrt_feeder_new['ratio_early'] = ratio_mrt_feeder_new['n_early'] / ratio_mrt_feeder_new['n']
     ratio_mrt_feeder_new['ratio_late'] = ratio_mrt_feeder_new['n_late'] / ratio_mrt_feeder_new['n']
 
+    df_202512 = pd.read_csv("dataset/MRT_monthly_bpi_202512.csv")
+    ratio_mrt_feeder_202512 = df_202512[['route_id', 'month', 'n', 'n_on_time', 'n_early', 'n_late']]
+
+    ratio_mrt_feeder_202512['ratio_on_time'] = ratio_mrt_feeder_202512['n_on_time'] / ratio_mrt_feeder_202512['n']
+    ratio_mrt_feeder_202512['ratio_early'] = ratio_mrt_feeder_202512['n_early'] / ratio_mrt_feeder_202512['n']
+    ratio_mrt_feeder_202512['ratio_late'] = ratio_mrt_feeder_202512['n_late'] / ratio_mrt_feeder_202512['n']
+
+    df_202601 = pd.read_csv("dataset/MRT_monthly_bpi_202601.csv")
+    ratio_mrt_feeder_202601 = df_202601[['route_id', 'month', 'n', 'n_on_time', 'n_early', 'n_late']]
+
+    ratio_mrt_feeder_202601['ratio_on_time'] = ratio_mrt_feeder_202601['n_on_time'] / ratio_mrt_feeder_202601['n']
+    ratio_mrt_feeder_202601['ratio_early'] = ratio_mrt_feeder_202601['n_early'] / ratio_mrt_feeder_202601['n']
+    ratio_mrt_feeder_202601['ratio_late'] = ratio_mrt_feeder_202601['n_late'] / ratio_mrt_feeder_202601['n']
+
+    df_202602 = pd.read_csv("dataset/MRT_monthly_bpi_202602.csv")
+    ratio_mrt_feeder_202602 = df_202602[['route_id', 'month', 'n', 'n_on_time', 'n_early', 'n_late']]
+
+    ratio_mrt_feeder_202602['ratio_on_time'] = ratio_mrt_feeder_202602['n_on_time'] / ratio_mrt_feeder_202602['n']
+    ratio_mrt_feeder_202602['ratio_early'] = ratio_mrt_feeder_202602['n_early'] / ratio_mrt_feeder_202602['n']
+    ratio_mrt_feeder_202602['ratio_late'] = ratio_mrt_feeder_202602['n_late'] / ratio_mrt_feeder_202602['n']
+
     ratio_mrt_feeder = ratio_mrt_feeder.round({
         'ratio_on_time': 4,
         'ratio_late': 4,
@@ -61,7 +121,26 @@ def ratio_mrt_feeder():
         'ratio_early': 4
     })
 
-    combined = pd.concat([ratio_mrt_feeder, ratio_mrt_feeder_new], ignore_index=True)
+    ratio_mrt_feeder_202512 = ratio_mrt_feeder_202512.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    ratio_mrt_feeder_202601 = ratio_mrt_feeder_202601.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    ratio_mrt_feeder_202602 = ratio_mrt_feeder_202602.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+
+    combined = pd.concat([ratio_mrt_feeder, ratio_mrt_feeder_new, ratio_mrt_feeder_202512, ratio_mrt_feeder_202601, ratio_mrt_feeder_202602], ignore_index=True)
 
     return combined
 
@@ -69,10 +148,16 @@ def ratio_mrt_feeder():
 def unique_months():
     df = pd.read_csv("dataset/BPI_Rapid_KL_relative.csv")
     df_new = pd.read_csv("dataset/BPI_Rapid_KL_new.csv")
+    df_202512 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202512.csv")
+    df_202601 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202601.csv")
+    df_202602 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202602.csv")
     
     combined_months = pd.concat([
         df['month'],
-        df_new['month']
+        df_new['month'],
+        df_202512['month'],
+        df_202601['month'],
+        df_202602['month']
     ])
 
     unique_months = sorted(combined_months.unique())
@@ -83,13 +168,23 @@ def unique_months():
 def bpi_rapid_kl():
     df = pd.read_csv("dataset/BPI_Rapid_KL_relative.csv")
     df_new = pd.read_csv("dataset/BPI_Rapid_KL_new.csv")
+    df_202512 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202512.csv")
+    df_202601 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202601.csv")
+    df_202602 = pd.read_csv("dataset/Rapid_kl_monthly_bpi_202602.csv")
+
     bpi_rapid_kl = df[['route_short_name', 'route_long_name','mae_ratio','otp', 'month', 'I_hat']].rename(columns={'I_hat': 'BPI', 'mae_ratio':'rMAE','otp':'OTP'})
     bpi_rapid_kl_new = df_new[['route_short_name', 'route_long_name','mae_ratio','otp', 'month', 'I_hat']].rename(columns={'I_hat': 'BPI', 'mae_ratio':'rMAE','otp':'OTP'})
+    bpi_rapid_202512 = df_202512[['route_short_name', 'route_long_name','mae_ratio','otp', 'month', 'BPI']].rename(columns={'mae_ratio':'rMAE','otp':'OTP'})
+    bpi_rapid_202601 = df_202601[['route_short_name', 'route_long_name','mae_ratio','otp', 'month', 'BPI']].rename(columns={'mae_ratio':'rMAE','otp':'OTP'})
+    bpi_rapid_202602 = df_202602[['route_short_name', 'route_long_name','mae_ratio','otp', 'month', 'BPI']].rename(columns={'mae_ratio':'rMAE','otp':'OTP'})
 
     bpi_rapid_kl['rMAE'] = bpi_rapid_kl['rMAE'].clip(upper=1)
     bpi_rapid_kl_new['rMAE'] = bpi_rapid_kl_new['rMAE'].clip(upper=1)
+    bpi_rapid_202512['rMAE'] =  bpi_rapid_202512['rMAE'].clip(upper=1)
+    bpi_rapid_202601['rMAE'] =  bpi_rapid_202601['rMAE'].clip(upper=1)
+    bpi_rapid_202602['rMAE'] =  bpi_rapid_202602['rMAE'].clip(upper=1)
 
-    combined = pd.concat([bpi_rapid_kl, bpi_rapid_kl_new], ignore_index=True)
+    combined = pd.concat([bpi_rapid_kl, bpi_rapid_kl_new, bpi_rapid_202512, bpi_rapid_202601, bpi_rapid_202602], ignore_index=True)
 
     return combined
 
@@ -97,12 +192,22 @@ def bpi_rapid_kl():
 def bpi_mrt_feeder():
     df = pd.read_csv("dataset/BPI_MRT_Feeder_relative.csv")
     df_new = pd.read_csv("dataset/BPI_MRT_Feeder_new.csv")
+    df_202512 = pd.read_csv("dataset/MRT_monthly_bpi_202512.csv")
+    df_202601 = pd.read_csv("dataset/MRT_monthly_bpi_202601.csv")
+    df_202602 = pd.read_csv("dataset/MRT_monthly_bpi_202602.csv")
+
     bpi_mrt_feeder = df[['route_id', 'mae_ratio','otp', 'month', 'I_hat']].rename(columns={'I_hat': 'BPI', 'mae_ratio':'rMAE','otp':'OTP' })
     bpi_mrt_feeder_new = df_new[['route_id', 'mae_ratio','otp', 'month', 'I_hat']].rename(columns={'I_hat': 'BPI', 'mae_ratio':'rMAE','otp':'OTP' })
+    bpi_mrt_202512 = df_202512[['route_id', 'mae_ratio','otp', 'month', 'I_hat']].rename(columns={'mae_ratio':'rMAE','otp':'OTP'})
+    bpi_mrt_202601 = df_202601[['route_id', 'mae_ratio','otp', 'month', 'I_hat']].rename(columns={'mae_ratio':'rMAE','otp':'OTP'})
+    bpi_mrt_202602 = df_202602[['route_id', 'mae_ratio','otp', 'month', 'I_hat']].rename(columns={'mae_ratio':'rMAE','otp':'OTP'})
 
     bpi_mrt_feeder['rMAE'] = bpi_mrt_feeder['rMAE'].clip(upper=1)
     bpi_mrt_feeder_new['rMAE'] = bpi_mrt_feeder_new['rMAE'].clip(upper=1)
+    bpi_mrt_202512['rMAE'] = bpi_mrt_202512['rMAE'].clip(upper=1)
+    bpi_mrt_202601['rMAE'] = bpi_mrt_202601['rMAE'].clip(upper=1)
+    bpi_mrt_202602['rMAE'] = bpi_mrt_202602['rMAE'].clip(upper=1)
 
-    combined = pd.concat([bpi_mrt_feeder, bpi_mrt_feeder_new], ignore_index=True)
+    combined = pd.concat([bpi_mrt_feeder, bpi_mrt_feeder_new, bpi_mrt_202512, bpi_mrt_202601, bpi_mrt_202602], ignore_index=True)
 
     return combined
--- dataset/Stop_dt.py
+++ dataset/Stop_dt.py
@@ -9,6 +9,15 @@ def stop_mrt_feeder():
     df_new = pd.read_csv("dataset/Stop_OTP_MRT_Feeder_new.csv")
     stop_mrt_feeder_new = df_new[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
 
+    df_202512 = pd.read_csv("dataset/MRT_bus_stop_otp_202512.csv")
+    stop_mrt_feeder_202512 = df_202512[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    df_202601 = pd.read_csv("dataset/MRT_bus_stop_otp_202601.csv")
+    stop_mrt_feeder_202601 = df_202601[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    df_202602 = pd.read_csv("dataset/MRT_bus_stop_otp_202602.csv")
+    stop_mrt_feeder_202602 = df_202602[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
+
     stop_mrt_feeder['total'] = stop_mrt_feeder['on time'] + stop_mrt_feeder['late'] + stop_mrt_feeder['early']
     stop_mrt_feeder['ratio_on_time'] = stop_mrt_feeder['on time'] / stop_mrt_feeder['total']
     stop_mrt_feeder['ratio_early'] = stop_mrt_feeder['early'] / stop_mrt_feeder['total']
@@ -31,7 +40,40 @@ def stop_mrt_feeder():
         'ratio_early': 4
     })
 
-    combined = pd.concat([stop_mrt_feeder, stop_mrt_feeder_new], ignore_index=True)
+    stop_mrt_feeder_202512['total'] = stop_mrt_feeder_202512['on time'] + stop_mrt_feeder_202512['late'] + stop_mrt_feeder_202512['early']
+    stop_mrt_feeder_202512['ratio_on_time'] = stop_mrt_feeder_202512['on time'] / stop_mrt_feeder_202512['total']
+    stop_mrt_feeder_202512['ratio_early'] = stop_mrt_feeder_202512['early'] / stop_mrt_feeder_202512['total']
+    stop_mrt_feeder_202512['ratio_late'] = stop_mrt_feeder_202512['late'] / stop_mrt_feeder_202512['total']
+
+    stop_mrt_feeder_202512 = stop_mrt_feeder_202512.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    stop_mrt_feeder_202601['total'] = stop_mrt_feeder_202601['on time'] + stop_mrt_feeder_202601['late'] + stop_mrt_feeder_202601['early']
+    stop_mrt_feeder_202601['ratio_on_time'] = stop_mrt_feeder_202601['on time'] / stop_mrt_feeder_202601['total']
+    stop_mrt_feeder_202601['ratio_early'] = stop_mrt_feeder_202601['early'] / stop_mrt_feeder_202601['total']
+    stop_mrt_feeder_202601['ratio_late'] = stop_mrt_feeder_202601['late'] / stop_mrt_feeder_202601['total']
+
+    stop_mrt_feeder_202601 = stop_mrt_feeder_202601.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    stop_mrt_feeder_202602['total'] = stop_mrt_feeder_202602['on time'] + stop_mrt_feeder_202602['late'] + stop_mrt_feeder_202602['early']
+    stop_mrt_feeder_202602['ratio_on_time'] = stop_mrt_feeder_202602['on time'] / stop_mrt_feeder_202602['total']
+    stop_mrt_feeder_202602['ratio_early'] = stop_mrt_feeder_202602['early'] / stop_mrt_feeder_202602['total']
+    stop_mrt_feeder_202602['ratio_late'] = stop_mrt_feeder_202602['late'] / stop_mrt_feeder_202602['total']
+
+    stop_mrt_feeder_202602 = stop_mrt_feeder_202602.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    combined = pd.concat([stop_mrt_feeder, stop_mrt_feeder_new, stop_mrt_feeder_202512, stop_mrt_feeder_202601, stop_mrt_feeder_202602], ignore_index=True)
 
     return combined
 
@@ -71,7 +113,58 @@ def stop_rapid_kl():
         'ratio_early': 4
     })
 
-    combined = pd.concat([stop_rapid_kl, stop_rapid_kl_new], ignore_index=True)
+    df_202512 = pd.read_csv("dataset/Rapid_kl_bus_stop_otp_202512.csv")
+    stop_rapid_kl_202512 = df_202512[['route_short_name','route_long_name', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    stop_rapid_kl_202512['route_id'] = stop_rapid_kl_202512['route_short_name'] + " (" + stop_rapid_kl_202512['route_long_name'] + ")"
+    stop_rapid_kl_202512 = stop_rapid_kl_202512[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    stop_rapid_kl_202512['total'] = stop_rapid_kl_202512['on time'] + stop_rapid_kl_202512['late'] + stop_rapid_kl_202512['early']
+    stop_rapid_kl_202512['ratio_on_time'] = stop_rapid_kl_202512['on time'] / stop_rapid_kl_202512['total']
+    stop_rapid_kl_202512['ratio_early'] = stop_rapid_kl_202512['early'] / stop_rapid_kl_202512['total']
+    stop_rapid_kl_202512['ratio_late'] = stop_rapid_kl_202512['late'] / stop_rapid_kl_202512['total']
+
+    stop_rapid_kl_202512 = stop_rapid_kl_202512.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    df_202601 = pd.read_csv("dataset/Rapid_kl_bus_stop_otp_202601.csv")
+    stop_rapid_kl_202601 = df_202601[['route_short_name','route_long_name', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    stop_rapid_kl_202601['route_id'] = stop_rapid_kl_202601['route_short_name'] + " (" + stop_rapid_kl_202601['route_long_name'] + ")"
+    stop_rapid_kl_202601 = stop_rapid_kl_202601[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    stop_rapid_kl_202601['total'] = stop_rapid_kl_202601['on time'] + stop_rapid_kl_202601['late'] + stop_rapid_kl_202601['early']
+    stop_rapid_kl_202601['ratio_on_time'] = stop_rapid_kl_202601['on time'] / stop_rapid_kl_202601['total']
+    stop_rapid_kl_202601['ratio_early'] = stop_rapid_kl_202601['early'] / stop_rapid_kl_202601['total']
+    stop_rapid_kl_202601['ratio_late'] = stop_rapid_kl_202601['late'] / stop_rapid_kl_202601['total']
+
+    stop_rapid_kl_202601 = stop_rapid_kl_202601.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    df_202602 = pd.read_csv("dataset/Rapid_kl_bus_stop_otp_202602.csv")
+    stop_rapid_kl_202602 = df_202602[['route_short_name','route_long_name', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    stop_rapid_kl_202602['route_id'] = stop_rapid_kl_202602['route_short_name'] + " (" + stop_rapid_kl_202602['route_long_name'] + ")"
+    stop_rapid_kl_202602 = stop_rapid_kl_202602[['route_id', 'stop_name', 'month', 'on time', 'early', 'late']]
+
+    stop_rapid_kl_202602['total'] = stop_rapid_kl_202602['on time'] + stop_rapid_kl_202602['late'] + stop_rapid_kl_202602['early']
+    stop_rapid_kl_202602['ratio_on_time'] = stop_rapid_kl_202602['on time'] / stop_rapid_kl_202602['total']
+    stop_rapid_kl_202602['ratio_early'] = stop_rapid_kl_202602['early'] / stop_rapid_kl_202602['total']
+    stop_rapid_kl_202602['ratio_late'] = stop_rapid_kl_202602['late'] / stop_rapid_kl_202602['total']
+
+    stop_rapid_kl_202602 = stop_rapid_kl_202602.round({
+        'ratio_on_time': 4,
+        'ratio_late': 4,
+        'ratio_early': 4
+    })
+
+    combined = pd.concat([stop_rapid_kl, stop_rapid_kl_new, stop_rapid_kl_202512, stop_rapid_kl_202601, stop_rapid_kl_202602], ignore_index=True)
 
     return combined
 
@@ -79,19 +172,15 @@ import pandas as pd
 
 def list_route():
     # Read datasets
-    df_mrt = pd.read_csv("dataset/Stop_OTP_MRT_Feeder_relative.csv")
-    df_kl = pd.read_csv("dataset/Stop_OTP_Rapid_KL_relative.csv")
-
-    df_mrt_new = pd.read_csv("dataset/Stop_OTP_MRT_Feeder_new.csv")
-    df_kl_new = pd.read_csv("dataset/Stop_OTP_Rapid_KL_new.csv")
+    df_mrt = pd.read_csv("dataset/MRT_bus_stop_otp_202602.csv")
+    df_kl = pd.read_csv("dataset/Rapid_kl_bus_stop_otp_202602.csv")
 
     # Ensure route_id exists for Rapid KL datasets
-    for df in [df_kl, df_kl_new]:
-        df["route_id"] = df["route_short_name"] + " (" + df["route_long_name"] + ")"
+    df_kl["route_id"] = df_kl["route_short_name"] + " (" + df_kl["route_long_name"] + ")"
 
     # Collect unique routes using set (simplest + fastest)
-    routes_kl = set(df_kl["route_id"]) | set(df_kl_new["route_id"])
-    routes_mrt = set(df_mrt["route_id"]) | set(df_mrt_new["route_id"])
+    routes_kl = set(df_kl["route_id"])
+    routes_mrt = set(df_mrt["route_id"])
 
     # Combine everything
     all_routes = sorted(routes_kl | routes_mrt)
